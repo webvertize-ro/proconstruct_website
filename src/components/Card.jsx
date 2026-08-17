e3742fecf3fb4ef2fe4d34c51edadcb15c1ada7e
webvertize-ro <contact@webvertize.ro>
2026-08-17 23:59:59 +0300

    further changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -52,7 +52,7 @@ const TextContent = styled.div`
 
 const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
   font-size: 2.2rem;
-  color: rgba(16, 31, 28, 1);
+  color: rgba(26, 46, 42, 1);
 `;
 
 const StyledH4 = styled.h4`
